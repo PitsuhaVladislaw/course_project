ea54ea45d4ccf1f0814064e263a63fecab0220a8
Vladislaw <vladikpizza02@gmail.com>
2023-05-11 21:58:13 +0300

    feat: add backend's part

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
 
+const fs = require("fs");
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const logger = require("morgan");
+const { ApolloServer, gql } = require('apollo-server-express');
 const app = express();
 
-const post = process.env.PORT || 3001;
+const port = process.env.PORT || 3001;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
@@ -15,10 +17,21 @@ const corsOption = {
     origin: "*",
     methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
     credential: true,
-    exposeHeaders: ["X-auth-token"]
+    exposeHeaders: ["X-auth-token"],
 }
 
-app.use(cors(corsOption));
-app.listen(post, () => {
-    console.log(`Servar work at ${post}`)
+app.listen(port, () => {
+    console.log(`Servar work at ${port}`)
 });
+
+
+app.use(cors(corsOption));
+
+const typeDefs = gql(
+    fs.readFileSync("./schema.graphql", {encoding: "utf-8"})
+);
+
+const resolvers = require("./resolvers"); 
+
+const apolloServer = new ApolloServer({typeDefs, resolvers});
+apolloServer.applyMiddleware({app, path: "/grapgql"});
